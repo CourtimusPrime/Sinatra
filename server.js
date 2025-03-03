81f020abba0b0fff876091c665a44f9c01ca4d05
CourtimusPrime <dashdale02@gmail.com>
2025-03-03 18:14:13 -0500

    Moved files around so now everything can run smoothly

--- server.js
+++ server.js
@@ -51,6 +51,10 @@ app.get('/callback', async (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 });
